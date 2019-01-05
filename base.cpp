0377a9a515b4e3491a7d54d73f25097ce0097b87
maxprus <maxprus2@gmail.com>
2019-01-05 17:40:48 +0100

    1

--- base.cpp
+++ base.cpp
@@ -3,12 +3,35 @@
 using namespace std;
 
 CBase::CBase(int maxSensors):
-maxSensors(maxSensors)
-{
-	Sensors = new CSensor*[maxSensors];
-}
+maxSensors(maxSensors),
+Sensors(new CSensor*[maxSensors])
+{}
+
 CBase::~CBase(){
 	delete Sensors;
+}
+
+CBase::CBase(const CBase& old):
+maxSensors(old.maxSensors),
+numSensors(old.numSensors),
+Sensors(new CSensor*[maxSensors])
+{
+	for(int i=0; i< maxSensors; i++)
+		Sensors[i] = old.Sensors[i];
+}
+
+const CBase& CBase::operator=(const CBase& old)
+{
+	if(this != &old)
+	{
+		delete Sensors;
+		maxSensors = old.maxSensors;
+		numSensors = old.numSensors;
+		Sensors = new CSensor*[maxSensors];
+		for(int i=0; i< maxSensors; i++)
+			Sensors[i] = old.Sensors[i];
+	}
+	return *this;
 }
 
 void CBase::setSensor(CSensor *pSensor)
@@ -16,7 +39,8 @@ void CBase::setSensor(CSensor *pSensor)
 	if(numSensors < maxSensors)
 		Sensors[numSensors++] = pSensor;
 	else
-		cout << "Maximum number of sensors reached !" << endl;
+		throw CException("Too many sensors!");
+		//cout << "Maximum number of sensors reached !" << endl;
 }
 
 void CBase::displayMeasurements()
--- base.hpp
+++ base.hpp
@@ -1,15 +1,19 @@
 #ifndef _base__hpp__
 #define _base__hpp__
 #include "sensors.hpp"
+#include "exception.h"
 
 class CBase
 {
-	public:
+public:
 	CBase(int maxSensors);
 	~CBase();
+	CBase(const CBase& old);
 	void setSensor(CSensor *pSensor);
 	void displayMeasurements();
-	private:
+	const CBase& operator=(const CBase& old);
+	
+private:
 	int maxSensors;
 	int numSensors =0;
    	CSensor** Sensors;
